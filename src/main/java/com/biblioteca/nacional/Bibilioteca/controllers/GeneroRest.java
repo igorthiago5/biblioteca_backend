05d8bd5e0f68ff841116c92ed239dec07409764c
igorthiago5 <igorthiago255@hotmail.com>
2025-07-05 01:22:06 -0300

    Adição dos Services de Genero para salvar

--- src/main/java/com/biblioteca/nacional/Bibilioteca/controllers/GeneroRest.java
+++ src/main/java/com/biblioteca/nacional/Bibilioteca/controllers/GeneroRest.java
@@ -1,6 +1,8 @@
 package com.biblioteca.nacional.Bibilioteca.controllers;
 
 import com.biblioteca.nacional.Bibilioteca.entities.Genero;
+import com.biblioteca.nacional.Bibilioteca.service.GeneroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -9,9 +11,29 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/genero")
 public class GeneroRest {
+    private String json;
+    @Autowired
+    GeneroService service;
+
     @CrossOrigin
     @PostMapping("/add")
     public ResponseEntity<?> addGenero(@RequestBody Genero genero){
-        return ResponseEntity.status(HttpStatus.OK).body(genero);
+        try{
+            service.salvarGenero(genero);
+            json  = """
+                    {
+                       "result":true
+                    }
+                    """;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            json = """
+                    {
+                        "result":false,
+                        "erro":%s
+                    }
+                    """.formatted(e.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(json);
     }
 }
--- src/main/java/com/biblioteca/nacional/Bibilioteca/service/GeneroService.java
+++ src/main/java/com/biblioteca/nacional/Bibilioteca/service/GeneroService.java
@@ -1,7 +1,16 @@
 package com.biblioteca.nacional.Bibilioteca.service;
 
+import com.biblioteca.nacional.Bibilioteca.entities.Genero;
+import com.biblioteca.nacional.Bibilioteca.repositorys.GeneroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GeneroService {
+    @Autowired
+    GeneroRepository generoRepository;
+
+    public Genero salvarGenero(Genero genero){
+        return generoRepository.save(genero);
+    }
 }
